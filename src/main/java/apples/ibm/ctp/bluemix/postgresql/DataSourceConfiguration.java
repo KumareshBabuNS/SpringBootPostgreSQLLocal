613eb2bf7205a387758780c0800383c96283749e
pasapicella <pas.apicella@tpg.com.au>
2015-11-25 19:28:15 +1100

    setup to deploy to cloud (Bluemix)

--- src/main/java/apples/ibm/ctp/bluemix/postgresql/DataSourceConfiguration.java
+++ src/main/java/apples/ibm/ctp/bluemix/postgresql/DataSourceConfiguration.java
@@ -6,23 +6,22 @@ import org.springframework.cloud.Cloud;
 import org.springframework.cloud.CloudFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
 
 @Configuration
-public class DataSourceConfiguration
-{
+@Profile("cloud")
+public class DataSourceConfiguration {
 
     @Bean
-    public Cloud cloud()
-    {
+    public Cloud cloud() {
         return new CloudFactory().getCloud();
     }
 
     @Bean
     @ConfigurationProperties(DataSourceProperties.PREFIX)
-    public DataSource dataSource()
-    {
+    public DataSource dataSource() {
         return cloud().getSingletonServiceConnector(DataSource.class, null);
     }
 
